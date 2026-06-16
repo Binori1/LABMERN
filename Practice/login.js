13d473c42909e27bfa8786fa668af5f3ce0f8f6b
Subaish <subasishsamal111@gmail.com>
2026-06-16 15:04:52 +0530

    Push

--- Practice/login.js
+++ Practice/login.js
@@ -1,13 +1,20 @@
-// let findLoginForm = document.getElementById("loginForm");
-// findLoginForm.addEventListener("submit",(event) => {
-//     //get email id and trim it
-//     let findEmail = findLoginForm.getElementById("email");
-//     let valueofEmail = findEmail.value.trim();
-//     //get password and trim 
-//     let findPassword = findLoginForm.getElementById("password");
-//     let valueofPassword = findPassword.value.trim();
-//     if(valueofEmail === "" || valueofPassword === ""){
-//         event.preventDefault();
-//     }
+let submitButton = document.querySelector("#loginForm");
+submitButton.addEventListener("submit",(event) => {
+    let email = document.querySelector("#email");
+    let emailvalue = email.value.trim();
+    let password = document.querySelector("#password");
+    let passwordValue = password.value.trim();
+        if(emailvalue === "" && passwordValue ===""){
+             event.preventDefault();
+             alert("Email and password can't be empty")
+        }else if (emailvalue === ""){
+
+            event.preventDefault();
+             alert("Enter Email id")
+        }else if(passwordValue === ""){
+             event.preventDefault();
+             alert("Enter Password")
+        }
+    }
  
-// })
+)
