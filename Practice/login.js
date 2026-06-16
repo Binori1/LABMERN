09cc389a18206c2111b837c7e791bec844dfcfee
Subaish <subasishsamal111@gmail.com>
2026-06-16 10:22:29 +0530

    Push

---
+++ Practice/login.js
@@ -0,0 +1,13 @@
+// let findLoginForm = document.getElementById("loginForm");
+// findLoginForm.addEventListener("submit",(event) => {
+//     //get email id and trim it
+//     let findEmail = findLoginForm.getElementById("email");
+//     let valueofEmail = findEmail.value.trim();
+//     //get password and trim 
+//     let findPassword = findLoginForm.getElementById("password");
+//     let valueofPassword = findPassword.value.trim();
+//     if(valueofEmail === "" || valueofPassword === ""){
+//         event.preventDefault();
+//     }
+ 
+// })
